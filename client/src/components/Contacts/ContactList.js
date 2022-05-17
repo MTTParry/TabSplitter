54baf7a8179fb18ceff8dc2090a227b121fef097
MTTParry <michaela.t.t.parry@gmail.com>
2022-05-17 15:40:10 -0700

    more styling and removing <hr/>

--- client/src/components/Contacts/ContactList.js
+++ client/src/components/Contacts/ContactList.js
@@ -80,6 +80,7 @@ function ContactList() {
                 <li>
                   Email: <b>{contact.email}</b>
                 </li>
+                <br/>
                 <li>
                   How to pay them: <b>{contact.preferred_payment_method}</b>
                 </li>
@@ -103,7 +104,6 @@ function ContactList() {
                 DELETE {contact.first_name}
               </button>
               <div className="note">CAREFUL: Delete cannot be undone.</div>
-              <hr />
             </div>
           );
         }
