b67c7ecc322a516b7a61861b8ff5fb07ed7b220e
MTTParry <michaela.t.t.parry@gmail.com>
2022-05-16 18:35:31 -0700

    testing for db

--- server/db/db-connection.js
+++ server/db/db-connection.js
@@ -3,6 +3,7 @@ let DATABASE_URL;
 if (process.env.NODE_ENV === "production") {
   DATABASE_URL = process.env.DATABASE_URL;
   console.log("HELLO");
+  console.log(process.env.DATABASE_URL);
 } else {
   DATABASE_URL = process.env.LOCAL_DATABASE_URL;
 }
