54d7681bd463c539a7a0f178ea0212f3eb1778d9
MTTParry <michaela.t.t.parry@gmail.com>
2022-05-16 18:44:40 -0700

    testing 2

--- server/db/db-connection.js
+++ server/db/db-connection.js
@@ -1,14 +1,14 @@
 const { Pool } = require("pg");
-let DATABASE_URL;
+let DATABASE_URL_TEST;
 if (process.env.NODE_ENV === "production") {
-  DATABASE_URL = process.env.DATABASE_URL;
+  DATABASE_URL_TEST = process.env.DATABASE_URL;
   console.log("HELLO");
   console.log(process.env.DATABASE_URL);
 } else {
-  DATABASE_URL = process.env.LOCAL_DATABASE_URL;
+  DATABASE_URL_TEST = process.env.LOCAL_DATABASE_URL;
 }
 const db = new Pool({
-  connectionString: DATABASE_URL,
+  connectionString: DATABASE_URL_TEST,
   ssl: process.env.DATABASE_SSL != "false" && {
     rejectUnauthorized: false,
   },
